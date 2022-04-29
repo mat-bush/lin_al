11cb8cdf87da8fbb65e58c5d7539f8a6383328b0
mathew <mathew@mathews-MacBook-Pro.local>
2022-04-28 23:06:32 -0400

    added random matrix feature

--- lin_al/mat_gen.py
+++ lin_al/mat_gen.py
@@ -1,5 +1,6 @@
+from ctypes.wintypes import FLOAT
 import numpy as np
-
+import random as rd
 
 class mat_gen():
 
@@ -17,5 +18,31 @@ class mat_gen():
                     dia_ind = dia_ind + 1
 
         return out
+
+
+    def random(self,n,m,num_type = FLOAT, high = 10,low = -10):
+        out = np.zeros(n,m)
+
+        for i in range(0,n):
+            for j in range(0,m):
+                if num_type == 'int':
+                    out[i][j] = rd.randint(low,high) 
+                elif num_type == 'float':
+                    sign = np.randint(-1,1)
+                    out[i][j] = rd.random() * high * sign
+                else:
+                    sign = np.randint(-1,1)
+                    out[i][j] = rd.random() * high * sign
+
+    def matToId(self,mat):
+        shape = np.shape(mat)
+        out = np.zeros(shape[0],shape[1])
+
+        if shape[0] != shape[1]:
+            print("Error: Identity matrix must be square")
+        else:
+            for i in range(0,shape[0]):
+                for j in range(0,shape[1]):
+                    out[i][j] = 1
 
     
